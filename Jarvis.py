6b1fb2c1dba78dadfc113ee486d2b67ae93acd37
Nitin Bhandari <nitinbhandari.cs@gmail.com>
2019-10-27 02:42:46 -0700

    Edit Jarvis file

--- Jarvis.py
+++ Jarvis.py
@@ -1,72 +1,111 @@
+#
+#
 #!/usr/bin/env python
 # coding: utf-8
+# main() will be run when you invoke this action
+#
+# @param Cloud Functions actions accept a single parameter, which must be a JSON object.
+#
+# @return The output of this action, which must be a JSON object.
+#
 
 __author__ = 'Nitn Bhandari'
 __email__ = 'nitinbhandari.cs@gmail.com'
 
+import sys
 import requests
 from requests.exceptions import HTTPError
 import os
-from dotenv import load_dotenv
 
 
-class getDoctorInfo(object):
+class GetDoctorInfo(object):
     def __init__(self, address):
         self.address = address
 
-    def get_coordinates(self, address):
-        address = address.strip().replace(' ', '+')
+    def get_coordinates(self):
+        self.address = self.address.strip().replace(' ', '+')
 
         google_maps_api_key = 'AIzaSyCnlLqSJhK7PPgPaa69gj7UaHdeKHN7es0'
         google_maps_reverse_geocode_url = 'https://maps.googleapis.com/maps/api/geocode/json'
-        maps_PARAMS = {'address': self.address,
+        maps_params = {'address': self.address,
                        'key': google_maps_api_key}
         try:
-            maps_data = requests.get(url=google_maps_reverse_geocode_url, params=maps_PARAMS)
+            maps_data = requests.get(url=google_maps_reverse_geocode_url, params=maps_params)
         except HTTPError as http_err:
-            print('HTTP rror occured: {}'.format(http_err))
+            print('HTTP Error occurred: {}'.format(http_err))
 
         json_maps_data = maps_data.json()
         lat = json_maps_data['results'][0]['geometry']['location']['lat']
         long = json_maps_data['results'][0]['geometry']['location']['lng']
 
         coordinates = str(lat) + ', ' + str(long) + ', 5'
-        rerturn coordinates
+        return coordinates
 
     def get_doctors_info(self, coordinates):
         better_doctor_api_key = '3d75d15626810e1edf67533f5b18611e'
         better_doctor_url = 'https://api.betterdoctor.com/2016-03-01/doctors'
-        betterdoctor_PARAMS = {'user_key': better_doctor_api_key,
+        betterdoctor_params = {'user_key': better_doctor_api_key,
                                'location': coordinates,
-                               'limit': 5}
+                               'limit': 3}
 
         try:
-            betterdoctor_data = requests.get(url=better_doctor_url, params=betterdoctor_PARAMS)
+            betterdoctor_data = requests.get(url=better_doctor_url, params=betterdoctor_params)
         except HTTPError as http_err:
             print('HTTP rror occured: {}'.format(http_err))
 
         json_betterdotor_data = betterdoctor_data.json()
-        print(betterdoctor_data.json())
-        return None
 
-    def gen_resp(self, info):
-        pass
+        doctors_info = []
+
+        for res in json_betterdotor_data['data']:
+            # TODO: Check Best Practice
+            if 'practices' not in res or res.get('practices') is None:
+                continue
+
+            if 'accepts_new_patients' not in res['practices'][0] and res['practices'][0]['accepts_new_patients'] is None or False:
+                continue
+
+            if 'name' not in res['practices'][0] and res['practices'][0]['name'] is None:
+                continue
+            practice_name = res['practices'][0]['name']
+
+            if 'visit_address' not in res['practices'][0] or res['practices'][0]['visit_address'] is None:
+                continue
+
+            try:
+                address = res['practices'][0]['visit_address']['street'] + ', ' \
+                        + res['practices'][0]['visit_address']['city'] + ', ' \
+                        + res['practices'][0]['visit_address']['state'] + ', ' \
+                        + res['practices'][0]['visit_address']['zip']
+            except:
+                print('Address is not updated')
+
+            if 'phones' in res['practices'][0] and 'number' in res['practices'][0]['phones'][0]:
+                phone = res['practices'][0]['phones'][0]['number']
+
+            doctors_info.append(practice_name + ' located at ' + address + ' is currently accepting patients. '
+                                                                           'Their number is: ' + phone)
+
+        n_results = len(doctors_info)
+        if n_results > 0:
+            doctors_info.insert(0, 'Alright, I found have {} doctor/s in the specified area that are accepting new patients. Here are the results.'
+                            .format(n_results))
+        else:
+            doctors_info.insert('Unfortunately, I did not find any doctor in that area. Do you want to try a new location?')
+        return {"result": doctors_info}
 
     def run_pipeline(self):
         # Reverse geocode address to coordinates
         coordinates = self.get_coordinates()
 
         # get the doctors info based on coordinates
-        info = self.get_doctors_info(coordinates)
-
-        # generate responses that can be spoken in human language
-        responses = self.get_resp(info)
+        return self.get_doctors_info(coordinates)
 
 
-
-def main():
-    doc_info = getDoctorInfo(address)
-    doc_info.run_pipeline()
+def main(dict):
+    address = dict.get('address')
+    doc_info = GetDoctorInfo(address)
+    return doc_info.run_pipeline()
 
 
 if __name__ == "__main__":
